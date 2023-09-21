d80280c0d1d8e4524d3a522e28a78547df6a30aa
adelacvg <adelacvgaoiro@163.com>
2023-09-21 10:42:47 +0800

    Update model2.py

--- model2.py
+++ model2.py
@@ -52,80 +52,6 @@ from torch.nn.utils import weight_norm, remove_weight_norm, spectral_norm
 from commons import init_weights, get_padding
 from text import symbols, num_tones, num_languages
 
-class DurationDiscriminator(nn.Module):  # vits2
-    def __init__(
-        self, in_channels, filter_channels, kernel_size, p_dropout, gin_channels=0
-    ):
-        super().__init__()
-
-        self.in_channels = in_channels
-        self.filter_channels = filter_channels
-        self.kernel_size = kernel_size
-        self.p_dropout = p_dropout
-        self.gin_channels = gin_channels
-
-        self.drop = nn.Dropout(p_dropout)
-        self.conv_1 = nn.Conv1d(
-            in_channels, filter_channels, kernel_size, padding=kernel_size // 2
-        )
-        self.norm_1 = modules.LayerNorm(filter_channels)
-        self.conv_2 = nn.Conv1d(
-            filter_channels, filter_channels, kernel_size, padding=kernel_size // 2
-        )
-        self.norm_2 = modules.LayerNorm(filter_channels)
-        self.dur_proj = nn.Conv1d(1, filter_channels, 1)
-
-        self.pre_out_conv_1 = nn.Conv1d(
-            2 * filter_channels, filter_channels, kernel_size, padding=kernel_size // 2
-        )
-        self.pre_out_norm_1 = modules.LayerNorm(filter_channels)
-        self.pre_out_conv_2 = nn.Conv1d(
-            filter_channels, filter_channels, kernel_size, padding=kernel_size // 2
-        )
-        self.pre_out_norm_2 = modules.LayerNorm(filter_channels)
-
-        if gin_channels != 0:
-            self.cond = nn.Conv1d(gin_channels, in_channels, 1)
-
-        self.output_layer = nn.Sequential(nn.Linear(filter_channels, 1), nn.Sigmoid())
-
-    def forward_probability(self, x, x_mask, dur, g=None):
-        dur = self.dur_proj(dur)
-        x = torch.cat([x, dur], dim=1)
-        x = self.pre_out_conv_1(x * x_mask)
-        x = torch.relu(x)
-        x = self.pre_out_norm_1(x)
-        x = self.drop(x)
-        x = self.pre_out_conv_2(x * x_mask)
-        x = torch.relu(x)
-        x = self.pre_out_norm_2(x)
-        x = self.drop(x)
-        x = x * x_mask
-        x = x.transpose(1, 2)
-        output_prob = self.output_layer(x)
-        return output_prob
-
-    def forward(self, x, x_mask, dur_r, dur_hat, g=None):
-        x = torch.detach(x)
-        if g is not None:
-            g = torch.detach(g)
-            x = x + self.cond(g)
-        x = self.conv_1(x * x_mask)
-        x = torch.relu(x)
-        x = self.norm_1(x)
-        x = self.drop(x)
-        x = self.conv_2(x * x_mask)
-        x = torch.relu(x)
-        x = self.norm_2(x)
-        x = self.drop(x)
-
-        output_probs = []
-        for dur in [dur_r, dur_hat]:
-            output_prob = self.forward_probability(x, x_mask, dur, g)
-            output_probs.append(output_prob)
-
-        return output_probs
-
 
 class TransformerCouplingBlock(nn.Module):
     def __init__(
@@ -559,7 +485,7 @@ class PosteriorEncoder_unet(nn.Module):
         self.enc = UNet1DConditionModel(
             in_channels=hidden_channels,
             out_channels=out_channels,
-            block_out_channels=(hidden_channels//4,hidden_channels//2,hidden_channels,hidden_channels),
+            block_out_channels=(hidden_channels//4,hidden_channels*3//8,hidden_channels//2,hidden_channels//2),
             norm_num_groups=8,
             cross_attention_dim=hidden_channels,
             attention_head_dim=n_heads,
@@ -629,35 +555,6 @@ class PosteriorEncoder(nn.Module):
         m, logs = torch.split(stats, self.out_channels, dim=1)
         z = (m + torch.randn_like(m) * torch.exp(logs)) * x_mask
         return z, m, logs, x_mask
-
-
-
-class MultiPeriodDiscriminator(torch.nn.Module):
-    def __init__(self, use_spectral_norm=False):
-        super(MultiPeriodDiscriminator, self).__init__()
-        periods = [2, 3, 5, 7, 11]
-
-        discs = [DiscriminatorS(use_spectral_norm=use_spectral_norm)]
-        discs = discs + [
-            DiscriminatorP(i, use_spectral_norm=use_spectral_norm) for i in periods
-        ]
-        self.discriminators = nn.ModuleList(discs)
-
-    def forward(self, y, y_hat):
-        y_d_rs = []
-        y_d_gs = []
-        fmap_rs = []
-        fmap_gs = []
-        for i, d in enumerate(self.discriminators):
-            y_d_r, fmap_r = d(y)
-            y_d_g, fmap_g = d(y_hat)
-            y_d_rs.append(y_d_r)
-            y_d_gs.append(y_d_g)
-            fmap_rs.append(fmap_r)
-            fmap_gs.append(fmap_g)
-
-        return y_d_rs, y_d_gs, fmap_rs, fmap_gs
-
 
 class ReferenceEncoder(nn.Module):
     """
@@ -771,20 +668,20 @@ class VITS(nn.Module):
             p_dropout,
         )
 
-        # self.enc_q = PosteriorEncoder_unet(
-        #     spec_channels,
-        #     inter_channels,
-        #     hidden_channels,
-        #     spec_channels,
-        # )
-        self.enc_q = PosteriorEncoder(
+        self.enc_q = PosteriorEncoder_unet(
             spec_channels,
             inter_channels,
             hidden_channels,
-            5,
-            1,
-            16,
+            spec_channels,
         )
+        # self.enc_q = PosteriorEncoder(
+        #     spec_channels,
+        #     inter_channels,
+        #     hidden_channels,
+        #     5,
+        #     1,
+        #     16,
+        # )
         # if use_transformer_flow:
         #     self.flow = TransformerCouplingBlock(
         #         inter_channels,
@@ -819,8 +716,8 @@ class VITS(nn.Module):
         x, m_p, logs_p, x_mask = self.enc_p(
             x, x_lengths, tone, language
         )
-        z, m_q, logs_q, y_mask = self.enc_q(y, y_lengths)
-        # z, m_q, logs_q, y_mask = self.enc_q(y, y_lengths, y, y_lengths)
+        # z, m_q, logs_q, y_mask = self.enc_q(y, y_lengths)
+        z, m_q, logs_q, y_mask = self.enc_q(y, y_lengths, y, y_lengths)
         # z_p = self.flow(z, y_mask)
         z_p=z
 
@@ -1348,7 +1245,7 @@ class NaturalSpeech2(nn.Module):
         loss_diff = loss_diff.mean()
 
         l_length, loss_kl, loss_kl_ph = losses
-        loss = 45*loss_diff + l_length + loss_kl + loss_kl_ph
+        loss = loss_diff + l_length + 40*loss_kl + loss_kl_ph
 
         # cross entropy loss to codebooks
         # _, indices, _, quantized_list = encode(codes_padded,8,codec)
